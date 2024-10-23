0be96d001aa790f9ba1dbbf298f3714460a935d7
Uexcel2022 <excellenceudoka@gmail.com>
2024-10-23 10:46:39 +0100

    BaseEntity and AuditorAwareImpl

---
+++ src/main/java/com/uexcel/customer/entity/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.uexcel.customer.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Setter @Getter @ToString
+public class BaseEntity {
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+
+}
